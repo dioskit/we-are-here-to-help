c83c5ac3c3cbbd523edc21d4ffac35e2911afb3e
dioskit <154680296+dioskit@users.noreply.github.com>
2024-02-12 22:36:39 +0530

    commit by me
    first commit

---
+++ README.txt
@@ -0,0 +1,13 @@
+This is my attempt to write html for to learn its basics and to make a webpage that shows my college question paper and its solutions for data structures quetions in C language
+- write questions of 1st questions of 1st sessional
+- with each question write the soultion and them make a message box to suggest alternate approaches
+- if can collaborate with other guys
+- if possible write c language to wirte c porgram in html
+
+FONTS TO USE
+- not decided yet
+- most likely to use hevelectica
+
+CSS PART
+- if you know css write such that code cna be viewed in mobile phone and tablets
+- add more instructions.
---
+++ codewriter.c
@@ -0,0 +1,66 @@
+/**********************************************************************************
+this code is to read a code form a file and change it to html tsxt so that we can copy past it directly
+-help me imporve this code 
+-also we dont want this head part and body part of html all we need is to print the porgram part nothing else
+
+thank you commit 1.
+*************************************************************************************/
+
+#include <stdio.h>
+
+int main() {
+    // Open the input file in read mode
+    FILE *inputFile = fopen("text.txt", "r");
+    
+    if (inputFile == NULL) {
+        perror("Error opening file");
+        return 1;
+    }
+
+    // Open the output file in write mode
+    FILE *outputFile = fopen("output.txt", "w");
+
+    if (outputFile == NULL) {
+        perror("Error opening output file");
+        fclose(inputFile);
+        return 1;
+    }
+
+    // Write HTML header to the output file
+    fprintf(outputFile, "<!DOCTYPE html>\n<html>\n<head>\n<title>Your Title</title>\n</head>\n<body>\n");
+
+    // Process the content of the input file
+    int c;
+    while ((c = fgetc(inputFile)) != EOF) {
+        switch (c) {
+            case '<':
+                fprintf(outputFile, "&lt;");
+                break;
+            case '>':
+                fprintf(outputFile, "&gt;");
+                break;
+            case '&':
+                fprintf(outputFile, "&amp;");
+                break;
+            case '\t':
+                fprintf(outputFile, "&nbsp;&nbsp;&nbsp;&nbsp;");
+                break;
+            case '\n':
+                fprintf(outputFile, "<br>");
+                break;
+            default:
+                fputc(c, outputFile);
+        }
+    }
+
+    // Write HTML footer to the output file
+    fprintf(outputFile, "</body>\n</html>");
+
+    // Close the files
+    fclose(inputFile);
+    fclose(outputFile);
+
+    printf("File successfully converted to HTML.\n");
+
+    return 0;
+}
